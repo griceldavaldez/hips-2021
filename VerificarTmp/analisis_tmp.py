cdcf2e3384ab92da99b18c2252ceedd1c6579c8a
griceldavaldez <gricel_valdez99@hotmail.com>
2021-07-27 15:23:09 -0400

    Se agregar funcionalidad para analizar directorio /tmp

--- VerificarTmp/analisis_tmp.py
+++ VerificarTmp/analisis_tmp.py
@@ -1,21 +1,29 @@
 
-from VerificarTmp.verificar_tmp import buscar_scripts, buscar_shells
 from Cuarentena.cuarentena import enviar_a_cuarentena
 from Logs.logs import echo_alarmas_log, echo_prevencion_log
 from Correo.correo import enviar_correo
 from BaseDatos.modelos import AlarmaPrevencion
 from BaseDatos.dao import insertarAlarmaPrevencion
-import subprocess 
-import datetime
+import subprocess, datetime
 
-
+#Funcion: analisis_tmp
+#	Invoca a las funciones encargadas de monitorear el directorio /tmp en busca de archivos sospechosos.
+#	(ver: buscar_shells y buscar_scripts)
+#
 def analisis_tmp(admin):
     buscar_shells('/tmp', admin)
     buscar_scripts('/tmp', admin)
 
 
+#Funcion: buscar_shells
+#	Inspecciona los archivos dentro de un directorio (de forma recursiva con find) buscando patrones
+#	de archivos shell (empiezan con #!).
+#	Si los encuentra, alerta y mueve el archivo al directorio de cuarentena.
+#	Guarda las alertas y las precauciones tomadas en los log correspondientes (Ver: echo_alarmaslog y echo_prevencionlog)
+#Parametros:
+#	DIR	(string) absolute path del directorio donde se buscaran los shells. Normalmente es /tmp
+#    admin (lista) lista con los datos del administrador para enviar correo
 def buscar_shells(DIR, admin):
-    fecha_hora = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S") # la hora en que se hizo el escaneo
     msg = ''
     p = subprocess.Popen("find "+DIR+" -type f", stdout=subprocess.PIPE, shell=True)
     (output, err) = p.communicate()
@@ -25,11 +33,12 @@ def buscar_shells(DIR, admin):
         (output, err) = cat.communicate()
         txt = output.decode("utf-8")
         if(txt !=''):
-            msg += 'Se encontró un posible script de shell en:' + linea + '\n'
+            fecha_hora = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S") # la hora en que se hizo el escaneo
+            msg += 'Se encontro un posible script de shell en:' + linea + '\n'
             enviar_a_cuarentena(linea)
             #print ('Se encontró un posible script de shell en:' + linea + "-> Archivo enviado a cuarentena. \ n")
             echo_alarmas_log( fecha_hora, "Shell encontrado" + linea, "analisis_tmp()", "")
-            echo_prevencion_log(fecha_hora, "Archivo" + linea + "movido a carpeta de cuarentena", "Shell encontrado en /tmp")
+            echo_prevencion_log(fecha_hora, "Archivo" + linea + "movido a carpeta de cuarentena", "Shell encontrado en "+ DIR)
     if msg!='':
         body = msg +"\nTodos los archivos se enviaron a cuarentena."
         enviar_correo(admin[0], admin[1], 'Tipo de alerta: Shells encontrados',body) # se envia el correo
@@ -42,5 +51,39 @@ def buscar_shells(DIR, admin):
         insertarAlarmaPrevencion(obj_alarm_prev)
 
 
-def buscar_scripts():
-   
+#Funcion: buscar_scripts
+#	Inspecciona las terminaciones de los archivos dentro de un directorio (de forma recursiva con find)
+#	buscando terminaciones clasicas de scripts (ejemplo: .py, .c, .exe, etc).
+#	Si los encuentra, alerta y mueve el archivo al directorio de cuarentena.
+#	Guarda las alertas y las precauciones tomadas en los log correspondientes (Ver: echo_alarmaslog y echo_prevencionlog)
+#Parametros:
+#	DIR	(string) absolute path del directorio donde se buscaran los scripts. Normalmente es /tmp
+#   admin (lista) lista con los datos del administrador para enviar correo
+
+def buscar_scripts(DIR, admin):
+    exten = ['py','c','cpp','ruby','sh','exe','php','java','pl']
+    cmd = "find "+DIR+" -type f "
+    for e in exten:
+        cmd+= "-iname '*."+e+"' -o -iname '*."+e+".*' -o "
+    if cmd!="find "+DIR+" -type f ":
+        cmd = cmd[:-4]
+    p =subprocess.Popen(cmd, stdout=subprocess.PIPE, shell=True)
+    (output, err) = p.communicate()
+    body = ''
+    scripts = output.decode("utf-8")
+    body = body + scripts
+    if body!='':
+        fecha_hora = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S") 
+        for linea in scripts.splitlines():
+            enviar_a_cuarentena(linea)
+            print ('Se encontró un posible script en:' + linea+  "-> Archivo enviado a cuarentena. \n")
+            echo_alarmas_log(fecha_hora, "Script encontrado "+linea + "en " + DIR, 'analisis_tmp()',"")
+            echo_prevencion_log("Archivo "+linea+" movido a la carpeta de cuarentena","Script encontrado en " +DIR)
+        body = 'Se encontraron posibles archivos de script: \n' +body +"\nTodos los archivos se enviaron a cuarentena."
+        enviar_correo(admin[0], admin[1], 'Tipo de alerta: Scripts encontrados', body)
+        obj_alarm_prev = AlarmaPrevencion()
+        obj_alarm_prev.setFechaHora(fecha_hora)
+        obj_alarm_prev.setTipoEscaneo('analisis_tmp')
+        obj_alarm_prev.setResultado("Script encontrado "+linea + "en " + DIR)
+        obj_alarm_prev.setAccion("Archivo "+linea+" movido a la carpeta de cuarentena")
+        insertarAlarmaPrevencion(obj_alarm_prev)
