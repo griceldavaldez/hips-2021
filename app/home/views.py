52d273c7a59b8f0fc8683a6ea1abf58fd83219f6
Gricelda Valdez <gricel.va99@gmail.com>
2021-07-30 00:59:18 -0400

    Agregar enlace para escaneo

--- app/home/views.py
+++ app/home/views.py
@@ -1,10 +1,19 @@
 from flask import render_template
+import flask
 from flask_login import login_required
 
 from . import home
+import main
 
 
 @home.route('/')
 @login_required
 def homepage():
+    return render_template('home/index.html', title="HIPS")
+
+@home.route('/scan')
+@login_required
+def iniciar_escaneo():
+    main()
+    flask('Se inició el escaneo...')
     return render_template('home/index.html', title="HIPS")
