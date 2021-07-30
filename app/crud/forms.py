2d3af3b0d2dc6171003730b351fa8425343781f7
Gricelda Valdez <gricel.va99@gmail.com>
2021-07-30 00:38:36 -0400

    Corregir length de string.

--- app/crud/forms.py
+++ app/crud/forms.py
@@ -4,22 +4,22 @@ from wtforms.validators import DataRequired, Length
 
 
 class MD5SumForm(FlaskForm):
-    directorio = StringField('Directorio', validators=[DataRequired(), Length(255)])
+    directorio = StringField('Directorio', validators=[DataRequired(), Length(min=1,max=255)])
     submit = SubmitField('Confirmar')
 
 class AppPeligrosaForm(FlaskForm):
-    nombre_sniffer = StringField('Nombre Sniffer', validators=[DataRequired(), Length(32)])
+    nombre_sniffer = StringField('Nombre Sniffer', validators=[DataRequired(), Length(min=1, max=100)])
 
 class LimiteProcesoForm(FlaskForm):
-    nombre_proceso = StringField('Nombre Proceso', validators=[DataRequired(), Length(32)])
+    nombre_proceso = StringField('Nombre Proceso', validators=[DataRequired(), Length(min=1, max=100)])
     uso_cpu = IntegerField('Uso CPU', [DataRequired()])
     uso_memoria = IntegerField('Uso CPU', [DataRequired()])
     tiempo_maximo_ejecucion = IntegerField('Uso CPU', [DataRequired()])
 
 class GeneralForm(FlaskForm):
-    ip = StringField('IP', validators=[DataRequired(), Length(32)])
-    correo = StringField('Correo E.', validators=[DataRequired(), Length(100)])
-    contrasenha_correo = PasswordField('Password Correo', validators=[DataRequired(), Length(100)])
+    ip = StringField('IP', validators=[DataRequired(), Length(min=1, max=100)])
+    correo = StringField('Correo E.', validators=[DataRequired(), Length(min=1, max=100)])
+    contrasenha_correo = PasswordField('Password Correo', validators=[DataRequired(), Length(min=1, max=100)])
     uso_cpu_por_defecto = IntegerField('Uso CPU Default', [DataRequired()])
     uso_memoria_por_defecto = IntegerField('Uso Memoria Default', [DataRequired()])
     intento_maximo_ssh = IntegerField('Intento Máximo SSH', [DataRequired()])
